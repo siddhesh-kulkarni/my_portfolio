5244a3f56829b11618b33f86fb5523fe716dfa1f
Siddhesh Kulkarni <siddheshsanjaykulkarni1111@gmail.com>
2025-02-19 11:36:37 +0530

    Skills updated

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -3,11 +3,10 @@ import { BsPatchCheckFill } from "react-icons/bs";
 export default function Experience() {
   return (
     <section id="experience">
-      <h5>What Skills I Have</h5>
       <h2>My Skills</h2>
       <div class="container experience_container">
         <div class="experience_frontend">
-          <h3>Frontend Skills</h3>
+          <h3>Frontend</h3>
           <div class="experience_content">
             <article className="experience_details">
               <BsPatchCheckFill className="experience_details-icon"/>
@@ -48,7 +47,7 @@ export default function Experience() {
           </div>
         </div>
         <div class="experience_backend">
-          <h3>Backend Skills</h3>
+          <h3>Backend</h3>
           <div class="experience_content">
             <article className="experience_details">
               <BsPatchCheckFill className="experience_details-icon"/>
