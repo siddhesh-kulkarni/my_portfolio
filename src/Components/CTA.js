8bc424a804d40cec5d1104f5fd7943e78a370da8
Siddhesh Kulkarni <siddheshsanjaykulkarni1111@gmail.com>
2025-02-19 09:13:22 +0530

    Updated the Resume

--- src/Components/CTA.js
+++ src/Components/CTA.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Sid_Resume from '../../src/assets/Sid_Resume.pdf';
+import Siddhesh_Kulkarni from '../../src/assets/Siddhesh_Kulkarni.pdf';
 const CTA=()=>{
     return(
         <div className="cta">
-            <a href={Sid_Resume} download className="btn">Download CV</a>
+            <a href={Siddhesh_Kulkarni} download className="btn">Download CV</a>
             <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
     )
